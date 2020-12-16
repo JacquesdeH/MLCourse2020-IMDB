b00c02e6c380131c65f61ce890c2a7089e3230a2
JacquesdeH <jacquesdeh@buaa.edu.cn>
2020-12-16 16:58:23 +0800

    Add ensembled model | 95.45%

--- Ensembler.py
+++ Ensembler.py
@@ -18,6 +18,7 @@ submissions = {
     "submission-20201215-082241-RoBERTa-Cumulbatch-Alldata-Warmup-Batchlarger-MoreEpoch": 95.50,
     "submission-20201215-110700-RoBERTa-Cumulbatch-Alldata-Warmup-Batchlarger-MoreEpoch": 95.41,
     "submission-20201216-000133-RoBERTa-Cumulbatch-Alldata-Warmup-Batchlarger-MoreEpoch": 95.24,
+    "submission-20201216-142158-RoBERTa-Cumulbatch-Alldata-Warmup-Batchlarger-MoreEpoch": 95.45,
 }
 
 emotions = ["positive", "negative"]
